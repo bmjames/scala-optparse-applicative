e2e826bc255f002af6cd634c516a39dafc99accf
Ben James <ben.james@guardian.co.uk>
2014-09-22 20:41:39 +0100

    Doc Chunk operations

--- src/main/scala/net/bmjames/opts/help/Chunk.scala
+++ src/main/scala/net/bmjames/opts/help/Chunk.scala
@@ -3,6 +3,8 @@ package net.bmjames.opts.help
 import scalaz.{Applicative, Monoid, MonadPlus}
 import scalaz.std.list._
 import scalaz.std.option._
+
+import scalaz.syntax.std.option._
 import scalaz.syntax.monadPlus._
 import scalaz.syntax.foldable._
 
@@ -10,9 +12,13 @@ import org.kiama.output.PrettyPrinter.Doc
 import org.kiama.output.{PrettyPrinter => PP}
 
 /** The free monoid on a semigroup A */
-case class Chunk[A](run: Option[A])
+final case class Chunk[A](run: Option[A]) {
+  def isEmpty: Boolean = run.isEmpty
+}
 
 object Chunk {
+
+  def empty[A]: Chunk[A] = Chunk(None)
 
   implicit val chunkMonadPlus: MonadPlus[Chunk] =
     new MonadPlus[Chunk] {
@@ -20,7 +26,7 @@ object Chunk {
         Chunk(Some(a))
 
       def empty[A]: Chunk[A] =
-        Chunk(None)
+        Chunk.empty
 
       def bind[A, B](fa: Chunk[A])(f: A => Chunk[B]): Chunk[B] =
         Chunk(fa.run.flatMap(f andThen (_.run)))
@@ -31,10 +37,9 @@ object Chunk {
 
   implicit def chunkMonoid[A](implicit A: Monoid[A]): Monoid[Chunk[A]] =
     new Monoid[Chunk[A]] {
-      def zero: Chunk[A] =
-        Chunk(None)
+      def zero: Chunk[A] = Chunk.empty
       def append(f1: Chunk[A], f2: => Chunk[A]): Chunk[A] =
-        chunked(A.append)(f1, f2)
+        chunked[A]((f1, f2) => A.append(f1, f2))(f1, f2)
     }
 
   implicit val docMonoid: Monoid[Doc] =
@@ -44,7 +49,7 @@ object Chunk {
     }
 
   /** Given a semigroup structure on A, return a monoid structure on Chunk[A] */
-  def chunked[A](f: (A, => A) => A): (Chunk[A], => Chunk[A]) => Chunk[A] =
+  def chunked[A](f: (A, A) => A): (Chunk[A], Chunk[A]) => Chunk[A] =
     {
       case (Chunk(None), y) => y
       case (x, Chunk(None)) => x
@@ -58,16 +63,45 @@ object Chunk {
       case as  => as.foldMap().point[Chunk]
     }
 
+  implicit class DocChunkSyntax(self: Chunk[Doc]) {
+
+    /** Concatenate two Chunks with a space in between. */
+    def <<+>>(that: Chunk[Doc]): Chunk[Doc] =
+      chunked[Doc](_ <+> _)(self, that)
+
+    /** Concatenate two Chunks with a softline in between */
+    def <</>>(that: Chunk[Doc]): Chunk[Doc] =
+      chunked[Doc](_ </> _)(self, that)
+  }
+
+  /** Concatenate Chunks vertically. */
+  def vcatChunks(chunks: List[Chunk[Doc]]): Chunk[Doc] =
+    chunks.foldRight(Chunk.empty[Doc])(chunked(_ <@> _))
+
+  /** Concatenate Chunks vertically separated by empty lines. */
+  def vsepChunks(chunks: List[Chunk[Doc]]): Chunk[Doc] =
+    chunks.foldRight(Chunk.empty[Doc])(chunked((x, y) => x <@> PP.empty <@> y))
+
+  def extractChunk[A : Monoid](chunk: Chunk[A]): A =
+    chunk.run.orZero
+
   def words(s: String): List[String] =
     s.split("\\s+").toList
 
   def stringChunk(s: String): Chunk[Doc] =
     s match {
-      case "" => Monoid[Chunk[Doc]].zero
+      case "" => Chunk.empty
       case s  => Applicative[Chunk].pure(PP.string(s))
     }
 
   def paragraph(s: String): Chunk[Doc] =
-    words(s).foldRight(Monoid[Chunk[Doc]].zero)((c, cs) => chunked[Doc](_ </> _)(stringChunk(c), cs))
+    words(s).foldRight(Chunk.empty[Doc])((c, cs) => chunked[Doc](_ </> _)(stringChunk(c), cs))
 
+  def tabulate(table: List[(Doc, Doc)], size: Int = 24): Chunk[Doc] =
+    table match {
+      case Nil => Chunk.empty
+      case xs  => Applicative[Chunk].pure(PP.vcat(
+        for ((k, v) <- table) yield PP.indent(PP.padtobreak(size, k <+> v), 2)
+      ))
+    }
 }
