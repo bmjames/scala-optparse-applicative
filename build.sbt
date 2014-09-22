c2dd800ebad1a951000a06f12df24003cde4c04e
Ben James <ben.james@which.co.uk>
2014-09-22 16:53:07 +0100

    Funky chunks

--- build.sbt
+++ build.sbt
@@ -1,1 +1,3 @@
 libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.1.0"
+
+libraryDependencies += "com.googlecode.kiama" %% "kiama" % "1.7.0"
--- src/main/scala/net/bmjames/opts/help/Chunk.scala
+++ src/main/scala/net/bmjames/opts/help/Chunk.scala
@@ -1,7 +1,11 @@
 package net.bmjames.opts.help
 
-import scalaz.MonadPlus
-import scalaz.syntax.plus._
+import scalaz.{Applicative, Monoid, MonadPlus}
+import scalaz.std.list._
+import scalaz.syntax.monadPlus._
+import scalaz.syntax.foldable._
+
+import org.kiama.output.PrettyPrinter.{Doc, string}
 
 /** The free monoid on a semigroup A */
 case class Chunk[A](run: Option[A])
@@ -23,5 +27,35 @@ object Chunk {
         ???
     }
 
+  implicit def chunkMonoid[A](implicit A: Monoid[A]): Monoid[Chunk[A]] = ???
+
+  implicit val docMonoid: Monoid[Doc] = ???
+
+  /** Given a semigroup structure on A, return a monoid structure on Chunk[A] */
+  def chunked[A](f: (A, A) => A): (Chunk[A], Chunk[A]) => Chunk[A] =
+    {
+      case (Chunk(None), y) => y
+      case (x, Chunk(None)) => x
+      case (Chunk(Some(x)), Chunk(Some(y))) => Chunk(Some(f(x, y)))
+    }
+
+  /** Concatenate a list into a Chunk. */
+  def listToChunk[A: Monoid](as: List[A]): Chunk[A] =
+    as match {
+      case Nil => Monoid[Chunk[A]].zero
+      case as  => as.foldMap().point[Chunk]
+    }
+
+  def words(s: String): List[String] =
+    s.split("\\s+").toList
+
+  def stringChunk(s: String): Chunk[Doc] =
+    s match {
+      case "" => Monoid[Chunk[Doc]].zero
+      case s  => Applicative[Chunk].pure(string(s))
+    }
+
+  def paragraph(s: String): Chunk[Doc] =
+    words(s).foldRight(Monoid[Chunk[Doc]].zero)((c, cs) => chunked[Doc](_ </> _)(stringChunk(c), cs))
 
 }
