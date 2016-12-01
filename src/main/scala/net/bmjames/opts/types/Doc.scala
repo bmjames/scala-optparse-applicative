172e2051700f05f38812bcc97381664e46ee39a3
Colt Frederickson <coltfred@gmail.com>
2016-12-01 14:26:05 -0700

    short circuit union if it fits the layout

--- src/main/scala/net/bmjames/opts/types/Doc.scala
+++ src/main/scala/net/bmjames/opts/types/Doc.scala
@@ -12,25 +12,25 @@ case object EmptyDoc extends Doc
 //A line break
 case object LineDoc extends Doc
 //Nest i spackes after any line break in the Doc.
-sealed abstract case class NestDoc (i: Int, d: Doc) extends Doc {
+sealed abstract case class NestDoc(i: Int, d: Doc) extends Doc {
   override def toString: String = s"NestDoc($i, Doc(...))"
 }
 //Document representing the some text. text *must* not contain
 //newlines.
-sealed abstract case class TextDoc (text: String) extends Doc
+sealed abstract case class TextDoc(text: String) extends Doc
 //Document which is the concatenation of 2 other documents.
-sealed abstract case class ConsDoc (d1: Doc, d2: Doc) extends Doc {
+sealed abstract case class ConsDoc(d1: Doc, d2: Doc) extends Doc {
   override def toString: String = "ConsDoc(...)"
 }
 //A choice between 2 Docs. d1 and d2 must be the same doc when flattened and d1 must be longer
 //Only exposed through group.
-sealed abstract case class UnionDoc (d1: Doc, d2: Doc) extends Doc {
+sealed abstract case class UnionDoc(d1: Doc, d2: Doc) extends Doc {
   override def toString: String = "UnionDoc(...)"
 }
 //produce a document from the current "column" value.
-sealed abstract case class ColumnDoc (f: Int => Doc) extends Doc
+sealed abstract case class ColumnDoc(f: Int => Doc) extends Doc
 //produce a document from the current nesting level.
-sealed abstract case class NestingDoc (f: Int => Doc) extends Doc
+sealed abstract case class NestingDoc(f: Int => Doc) extends Doc
 
 object Doc {
   import scala.language.implicitConversions
@@ -44,17 +44,17 @@ object Doc {
 
   //Primatives.
   //use `string` instead as it handles possible newlines.
-  private def text(s: String): Doc = new TextDoc(s){}
-  def nest(i: Int, d: Doc): Doc = new NestDoc(i, d){}
-  def append(d1: Doc, d2: Doc) = new ConsDoc(d1, d2){}
-  def column(f: Int => Doc): Doc = new ColumnDoc(f){}
-  def nesting(f: Int => Doc): Doc = new NestingDoc(f){}
+  private def text(s: String): Doc = new TextDoc(s) {}
+  def nest(i: Int, d: Doc): Doc = new NestDoc(i, d) {}
+  def append(d1: Doc, d2: Doc) = new ConsDoc(d1, d2) {}
+  def column(f: Int => Doc): Doc = new ColumnDoc(f) {}
+  def nesting(f: Int => Doc): Doc = new NestingDoc(f) {}
 
   //constants which are used frequently
   val space = text(" ")
   val empty: Doc = EmptyDoc
   val line: Doc = LineDoc
-  val softLine:Doc = group(line)
+  val softLine: Doc = group(line)
   //Fold up the docs using f, empty if it's Nil.
   def foldDoc(docs: Seq[Doc])(f: (Doc, Doc) => Doc): Doc = docs match {
     case Nil => empty
@@ -93,7 +93,7 @@ object Doc {
 
   def hang(i: Int, d: Doc): Doc = align(nest(i, d))
 
-  def indent(i:Int, d:Doc): Doc = hang(i, spaces(i).append(d))
+  def indent(i: Int, d: Doc): Doc = hang(i, spaces(i).append(d))
 
   def align(d: Doc): Doc = column(current => nesting(indent => nest(current - indent, d)))
 
@@ -101,7 +101,7 @@ object Doc {
 
   def enclose(l: Doc, m: Doc, r: Doc): Doc = append(append(l, m), r)
 
-  def group(d: Doc): Doc = new UnionDoc(flatten(d), d){}
+  def group(d: Doc): Doc = new UnionDoc(flatten(d), d) {}
 
   def prettyRender(cols: Int, d: Doc): String = RenderDoc.prettyPrintToString(RenderDoc.fromDoc(cols, d))
 
@@ -147,27 +147,27 @@ object RenderDoc {
   def fromDoc(width: Int, d: Doc): RenderDoc = findBest(width, 0, List((0, d)))
 
   //find the best layout using all the options in `l`. 
-  def findBest(allowedWidth: Int, amountTaken: Int, l: List[(Int, Doc)]): RenderDoc =
-    findBestCore(allowedWidth, amountTaken, l).run
+  def findBest(allowedWidth: Int, amountTaken: Int, l: List[(Int, Doc)]): RenderDoc = findBestCore(allowedWidth, amountTaken, l).run
 
   private def findBestCore(allowedWidth: Int, amountTaken: Int, l: List[(Int, Doc)]): Free.Trampoline[RenderDoc] = l match {
     case Nil => Trampoline.done(EmptyRenderDoc)
     case (i, EmptyDoc) :: tail => Trampoline.suspend(findBestCore(allowedWidth, amountTaken, tail))
     case (i, ConsDoc(d1, d2)) :: tail => Trampoline.suspend(findBestCore(allowedWidth, amountTaken, (i, d1) :: (i, d2) :: tail))
     case (i, NestDoc(j, d)) :: tail => Trampoline.suspend(findBestCore(allowedWidth, amountTaken, (i + j, d) :: tail))
-    case (i, TextDoc(s)) :: tail => 
+    case (i, TextDoc(s)) :: tail =>
       Trampoline.suspend(findBestCore(allowedWidth, amountTaken + s.length, tail).map[RenderDoc](TextRenderDoc(s, _)))
     case (i, LineDoc) :: tail => Trampoline.suspend(findBestCore(allowedWidth, i, tail).map(LineRenderDoc(i, _)))
     case (i, UnionDoc(d1, d2)) :: tail =>
-      for {
-        first <- Trampoline.suspend(findBestCore(allowedWidth, amountTaken, (i, d1) :: tail))
-        second <- Trampoline.suspend(findBestCore(allowedWidth, amountTaken, (i, d2) :: tail))
-      } yield better(allowedWidth, amountTaken, first, second)
+      Trampoline.suspend(findBestCore(allowedWidth, amountTaken, (i, d1) :: tail)).flatMap { d1Result =>
+        if (fits(allowedWidth - amountTaken, d1Result)) {
+          Trampoline.done(d1Result)
+        } else {
+          Trampoline.suspend(findBestCore(allowedWidth, amountTaken, (i, d2) :: tail))
+        }
+      }
     case (i, ColumnDoc(f)) :: tail => Trampoline.suspend(findBestCore(allowedWidth, amountTaken, (i, f(amountTaken)) :: tail))
     case (i, NestingDoc(f)) :: tail => Trampoline.suspend(findBestCore(allowedWidth, amountTaken, (i, f(i)) :: tail))
   }
-
-  def better(w: Int, k: Int, d1: RenderDoc, d2: RenderDoc): RenderDoc = if (fits(w - k, d1)) d1 else d2
 
   /**
    * Does the RenderDoc fit into the width.
